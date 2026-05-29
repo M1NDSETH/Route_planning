2c40896b8380a88e3bf3021782e77d8d7c49c5b1
Egor <gu1307303@gmail.com>
2026-05-29 15:21:45 +0300

    add: random obstacles generation to test

--- cpp/route_test.cpp
+++ cpp/route_test.cpp
@@ -2,9 +2,19 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <random>
 const double K_units = 20.0;
 
 int main() {
+
+    std::random_device rd; 
+    std::mt19937 gen(rd());
+    int min_x = 1, max_x = 999;
+    std::uniform_int_distribution<> distr_x(min_x, max_x);
+    int min_y = 1, max_y = 499;
+    std::uniform_int_distribution<> distr_y(min_y, max_y);
+
+
     std::vector<Point> targets;
     targets.push_back({999, 499});
     targets.push_back({500, 250});
@@ -18,12 +28,20 @@ int main() {
     Point start_pos = {1, 1};
     
     std::vector<Point> obstacles;
-    obstacles.push_back({999, 1});
-
+    for (int i = 0; i < 40; i++){
+        int x = distr_x(gen);
+        int y = distr_y(gen);
+        Point point = {x, y};
+        obstacles.push_back(point);
+    }
+    
     GRID grid(metres_to_grid_units(pool_length, K_units), metres_to_grid_units(pool_width, K_units), targets, obstacles);
     AUV VELT(start_pos, metres_to_grid_units(AUV_length, K_units),metres_to_grid_units(AUV_width, K_units));
     
-    obstacles_inflation(grid.field, grid, obstacles[0], VELT.radius);
+    for (int i = 0; i < 40; i++){
+        obstacles_inflation(grid.field, grid, obstacles[i], VELT.radius);
+    }
+    
 
     auto start = std::chrono::high_resolution_clock::now();
     std::vector<Point> full_route = VELT.build_full_route(targets, grid);
